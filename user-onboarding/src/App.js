893949b591aa360d4f62a88af0c86a09d2dd4880
Oluwa-femi <oluwatolablessing@gmail.com>
2019-09-20 07:51:13 +0100

    Error log

--- user-onboarding/src/App.js
+++ user-onboarding/src/App.js
@@ -1,24 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
+// import logo from './logo.svg';
 import './App.css';
+import FormByFormik from './Component/Form';
+import axios from "axios";
 
+const initialUser = {
+  name: '',
+  email: '',
+  password: '',
+};
+
+const usersApi = 'https://reqres.in/api/users_'
 function App() {
+
+  const [user, setUser] = useState(initialUser);
+  const [userList, setUserList] = useState([]);
+
+  const addUser = (formValues, actions) => {
+    const userToPost = {
+      name: formValues.name,
+      age: formValues.age,
+      password: formValues.password,
+    };
+    setUser(userToPost);
+    console.log("button was clicked")
+    axios.post(usersApi, userToPost)
+      .then(res => {
+        // res.data contains the newly created friend
+        const newUser = res.data;
+        setUserList(userList.concat(newUser));
+        actions.resetForm();
+      })
+      .catch(err => {
+        return err.message;
+      });
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <FormByFormik initialUser={initialUser} onSubmit={addUser}/>
     </div>
   );
 }
--- user-onboarding/src/Components/Form.js
+++ user-onboarding/src/Components/Form.js
@@ -8,22 +8,30 @@ const validationSchema = yup.object().shape({
     email: yup.string().email().required(),
     password: yup.string().required(),
 })
+
+const FormByFormik = (props) => {
+    const {onSubmit, initialUser} = props;
+
     return (
-        <Formik validationSchema={validationSchema} render={(props) =>{
+        <Formik onSubmit={onSubmit} initialValues={initialUser} validationSchema={validationSchema} render={(props) =>{
             return (
             <Form>
             <label>
               Name
-              <Field name="name" placeholder="name"></Field>
+                <Field name="name" placeholder="name"></Field>
+                <ErrorMessage name='name' component='div' />
             </label>
             <label>
-              Email<Field name="email" placeholder="email"></Field>
+                Email<Field name="email" placeholder="email"></Field>
+                <ErrorMessage name='email' component='div' />
             </label>
             <label>
-              Password<Field name="password" placeholder="password"></Field>
+                Password<Field name="password" placeholder="password"></Field>
+                <ErrorMessage name='password' component='div' />
             </label>
-            <Field name="checkbox" type="checkbox"></Field>
-            <button type="button">Submit</button>
+                <Field name="checkbox" type="checkbox"></Field>
+                <ErrorMessage name='checkbox' component='div' />
+            <button type="submit">Submit</button>
           </Form>
           )
         }}>
