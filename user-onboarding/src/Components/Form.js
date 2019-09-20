363e47b493ea95ad7ad78794edba1886666cb6e3
Oluwa-femi <oluwatolablessing@gmail.com>
2019-09-20 07:35:29 +0100

    Form created

---
+++ user-onboarding/src/Components/Form.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { Formik, Form, Field } from "formik";
+
+const FormByFormik = () => {
+
+    return (
+        <Formik>
+          <Form>
+            <label>
+              Name
+              <Field name="name" placeholder="name"></Field>
+            </label>
+            <label>
+              Email<Field name="email" placeholder="email"></Field>
+            </label>
+            <label>
+              Password<Field name="password" placeholder="password"></Field>
+            </label>
+            <Field name="checkbox" type="checkbox"></Field>
+            <button>Submit</button>
+          </Form>
+        </Formik>
+      );
+    };
+    export default FormByFormik;
